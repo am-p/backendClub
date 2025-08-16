b8972fe44564c630448584a9bdd636c36d024258
Ariel Marcelo Pardo <adoorno@gmail.com>
2025-08-16 20:02:13 -0300

    Finished first login/auth/jwt/register task.

--- app/src/main/kotlin/com/example/Application.kt
+++ app/src/main/kotlin/com/example/Application.kt
@@ -1,16 +1,19 @@
 package com.example
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
 import com.example.plugins.configureSerialization
 import com.example.plugins.configureSecurity
 import com.example.routes.configureRouting
+import com.example.data.UsersRepository
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureSerialization()
-        configureSecurity()     // installs JWT verification
-        configureRouting()      // defines /health, /login, /secure/me
-    }.start(wait = true)
+fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+    configureSerialization()
+    configureSecurity()
+    val users = UsersRepository()
+    configureRouting(users)
+
+
 }
